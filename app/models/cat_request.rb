4e8566999462782fe16bac1a8c494e5252b6abb4
Brit Butler <redline6561@gmail.com>
2015-02-18 22:37:31 -0500

    Add cat requests!

---
+++ app/models/cat_request.rb
@@ -0,0 +1,2 @@
+class CatRequest < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219033446) do
+ActiveRecord::Schema.define(version: 20150219033701) do
+
+  create_table "cat_requests", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
